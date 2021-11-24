0e308732065aa25b4e672fe5830e8c47dc6a2c76
Tejas <Tejas@Trupti-and-Tejas.lan>
2021-11-23 22:05:12 -0500

    Created this first code as a demo.

---
+++ E2ESeleniumJenkins/src/testcases/TC002.java
@@ -0,0 +1,23 @@
+package testcases;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//bare minimum program, as the focus is on e2e linking till Jenkins.
+
+import org.testng.annotations.*;
+
+public class TC002 {
+	
+	@Test
+	public void tc001() {
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("Search this");
+		driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
+		driver.quit();
+	}
+
+}
