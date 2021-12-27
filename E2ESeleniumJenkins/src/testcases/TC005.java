1b10a12ddbfabe079d238bbb27abf3077faa8d9d
Tejas <Tejas@Trupti-and-Tejas.lan>
2021-12-26 21:27:31 -0500

    new Test Cases added

---
+++ E2ESeleniumJenkins/src/testcases/TC005.java
@@ -0,0 +1,24 @@
+package testcases;
+
+import org.testng.annotations.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+//bare minimum program, as the focus is on e2e linking till Jenkins.
+
+public class TC005 {
+	
+	@Test
+	public void tc001() {
+		System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
+		driver.findElement(By.name("q")).sendKeys("Search this");
+		driver.findElement(By.name("q")).sendKeys(Keys.RETURN);
+		driver.quit();
+	}
+	
+	//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
+
+}
